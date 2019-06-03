63e4bf7c6030c199e6288f1c915e4f236965fe93
Timothy Lee <tlee728@berkeley.edu>
2019-06-03 11:15:36 -0700

    finished implementing all of the bee classes/subclasses

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,10 @@
+// bee subclass
 var Bee = function() {
-};
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+}
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
